2eb637e8b1361bf2b64db8a25b5967414572d4e5
RivusTheFerret <48331681+RivusTheFerret@users.noreply.github.com>
2019-04-30 14:16:17 -0400

    Add files via upload

--- fireAlarm.py
+++ fireAlarm.py
@@ -1,6 +1,3 @@
-### I believe I may not have made things clear enough in the code
-### IF anyone has any questions please ask me (Timothy)
-
 import random
 class student:
     def __init__(self, memoryDecay,caution):
@@ -8,8 +5,8 @@ class student:
         self.preparedness = 0 #percentile chance prepared
         self.response = 100 #percentile chance to respond
         self.day = 0 #the current day
-        self.cautiousLevel = caution #How much their response changes each day 
-        
+        self.cautiousLevel = caution #How much their response changes each day
+    
     def nextDay(self, isThereAnAlarm):
         self.day += 1
         
@@ -24,7 +21,7 @@ class student:
                 self.preparedness = 0
             if self.response >100:
                 self.response = 100
-                
+
 class fireDrill:
     def __init__(self,num):
         self.numberOfDrills = num
@@ -32,30 +29,57 @@ class fireDrill:
         self.drillDays = []
         for i in range(num):
             self.drillDays.append(random.randint(1,178))
-            
-    def isThisADrillDay(self,day):
-        if day in self.drillDays:
-            return True
+
+def isThisADrillDay(self,day):
+    if day in self.drillDays:
+        return True
         else:
             return False
+                
+                def avgDaysBetweenDrill(self):
+                    sumOfAvgDays = 0
+for i in range(len(self.drillDays)):
+    if(i)==0:
+        sumOfAvgDays+=(self.drillDays[i])
+            else:
+                if(self.drillDays[i]>self.drillDays[i-1]):
+                    sumOfAvgDays+=(self.drillDays[i]-self.drillDays[i-1])
+                else:
+                    sumOfAvgDays+=(self.drillDays[i-1]-self.drillDays[i])
+    return sumOfAvgDays/self.numberOfDrills
 
 #Assumption: on average, a student will not be prepared after one to two months of no drill
 #Assumption: on avergae, there are 178 days per school year
-    #This means, 2 months (on average 61 days) will be 45 days (subtracting saturdays and sundays)
-#Assumption: A student's caution levels can be between 100 (cautious all the time) and 0 (never cautious)  
-    #This having an average caution level of about 75
-    #100 caution means they will always respond, 0 caution means they will never respond
-    #75 caution means that when there's a drill, their response will drop 25%
-    #When there isn't a drill, their response will raise up by 2/3 caution * (100-preparedness)/100
-a = student(100/(45-random.randint(0,22)),75-random.randint(-25,75))
-fire = fireDrill(1)
+#This means, 2 months (on average 61 days) will be 45 days (subtracting saturdays and sundays)
+#Assumption: A student's caution levels can be between 100 (cautious all the time) and 0 (never cautious)
+#This having an average caution level of about 75
+#100 caution means they will always respond, 0 caution means they will never respond
+#75 caution means that when there's a drill, their response will drop 25%
+#When there isn't a drill, their response will raise up by 2/3 caution * (100-preparedness)/100
+def average(stud,typ,drill):
+    prepSum =0
+    respSum =0
+    for day in range(1,178):
+        prepSum+= ((int)(stud.preparedness))
+        respSum+= ((int)(stud.response))
+        stud.nextDay(drill.isThisADrillDay(day))
+    if typ == "preparedness":
+        return prepSum/178
+    else:
+        return respSum/178
 
-print(a.cautiousLevel)
-for i in range(1,178):
-    x = ""
-    a.nextDay(fire.isThisADrillDay(i))
-    x+="Day: "+(str)(i)+"\n"
-    x+="Drill: "+(str)(fire.isThisADrillDay(i))+"\n"
-    x+="Preparedness: "+(str)((int)(a.preparedness))+"\n"
-    x+="Response: "+(str)((int)(a.response))+"\n"
-    print(x)
+fire = fireDrill(50)
+studentList = [student(100/(45-random.randint(0,22)),75-random.randint(-25,75)) for i in range(19)]
+
+prepSum=0
+respSum=0
+for student in studentList:
+    prepSum+=average(student,"preparedness",fire)
+    respSum+=average(student,"resp",fire)
+
+print("Number of Drills: "+(str)(fire.numberOfDrills))
+print("Average number of Days between Drills: "+(str)(fire.avgDaysBetweenDrill()))
+print("\nAverage Prepareness of Students: "+(str)((int)(prepSum/len(studentList))))
+print("Average Responsenes of Students: "+(str)((int)(respSum/len(studentList))))
+
+
